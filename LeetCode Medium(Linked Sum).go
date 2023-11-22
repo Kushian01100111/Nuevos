a9788fae6fdd3557488cbafc59fa2de563fa5732
Kushian01100111 <comaspedro6@gmail.com>
2023-11-22 11:40:28 -0300

    22/11 Leetcode Linkedlist - Golang

--- LeetCode Medium(Linked Sum).go
+++ LeetCode Medium(Linked Sum).go
@@ -35,7 +35,6 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	dummy := &ListNode{}
 	current := dummy
 	carry := 0
-
 	for l1 != nil || l2 != nil || carry > 0 {
 		sum := carry
 
@@ -48,7 +47,6 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			sum += l2.Val
 			l2 = l2.Next
 		}
-
 		current.Next = &ListNode{Val: sum % 10}
 		current = current.Next
 		carry = sum / 10
