9a21695fe56319a6405307c5a9420d9ad386d315
Kushian01100111 <comaspedro6@gmail.com>
2023-12-27 20:35:45 -0300

    27/12 - Leetcode - Linked list - Reorder list

---
+++ Leetcode reOrderList LinkedList.go
@@ -0,0 +1,32 @@
+package kata
+
+type LinkedList struct {
+	Val  int
+	Next *LinkedList
+}
+
+func reOrderList(head *LinkedList) {
+	list := []int{}
+	temp := head
+
+	for temp != nil {
+		list = append(list, temp.Val)
+		temp = temp.Next
+	}
+
+	i, j := 0, len(list)-1
+	switchVal := true
+	for head != nil {
+		if switchVal {
+			head.Val = list[i]
+			i++
+			head = head.Next
+			switchVal = false
+		} else {
+			head.Val = list[j]
+			j--
+			head = head.Next
+			switchVal = true
+		}
+	}
+}
