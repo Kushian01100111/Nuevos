cc1379224462fdd04ae031bd1f47bbdcf2d3b8ed
Kushian01100111 <comaspedro6@gmail.com>
2024-03-31 13:46:13 -0300

    31/03 - Hard - Leetcode countSubarrays

---
+++ Leetcode Hard countSubArrays.go
@@ -0,0 +1,26 @@
+package kata
+
+func countSubarrays(nums []int, minK int, maxK int) int64 {
+	var ans int64
+	lastMin, lastMax := -1, -1
+	leftmost := 0
+	for i, d := range nums {
+		if d < minK || d > maxK {
+			leftmost = i + 1
+			lastMin = -1
+			lastMax = -1
+			continue
+		}
+		if d == minK {
+			lastMin = i
+		}
+		if d == maxK {
+			lastMax = i
+		}
+		if lastMin != -1 && lastMax != -1 {
+			ans += int64(min(lastMin, lastMax) - leftmost + 1)
+		}
+	}
+
+	return ans
+}
